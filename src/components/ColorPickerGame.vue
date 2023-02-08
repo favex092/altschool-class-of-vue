c864e3fd95ca7dd74c56b6394e4ea19d09a8df48
Nwabuisi Favour <favex2986@gmail.com>
2023-02-08 08:24:41 +0100

    assessment

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -3,31 +3,22 @@
 
   <div>{{ message }}</div>
 
-  <button v-for="color in colors" :key="color" @click="matchColor(color)">
+  <button
+    v-for="color in colors"
+    :key="color"
+    @click="matchColor(color)"
+    :style="{ background: color[color] }"
+  >
     {{ color }}
   </button>
 </template>
 
 <script>
-import { ref, reactive } from "@vue/reactivity";
-
+import ColorPickerCompose from "../composables/ColorPickerGame";
 export default {
   setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
-
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
-
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    };
-
+    const { colors, message, matchColor } = ColorPickerCompose();
+    console.log(colors[0]);
     return { colors, message, matchColor };
   },
 };
